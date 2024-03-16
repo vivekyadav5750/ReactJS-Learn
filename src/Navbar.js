4753ff40eafce7a15d8fa3c46168eba7defed6eb
Vivek <10Vivekkumaryadav@gmail.com>
2024-03-16 07:32:13 +0530

    use CSS Style component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,68 +1,66 @@
 import React from "react";
+import styled from "styled-components";
+
+// Style component (act as component)
+const Nav =  styled.div`
+    height: 70px;
+	background: linear-gradient(170deg, #1bc059, #0d47a1);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	position: relative;
+  
+`;
+
+const Title = styled.div`
+    font-size: 30px;
+    color: #fff;
+    font-weight: 600;
+    font-family: 'Times New Roman', Times, serif;
+    text-transform: uppercase;
+    margin-left: 20px ;
+    &:hover{color:#f00};
+
+`;
+
+
+const CartImg = styled.img`
+    height: 48px;
+    margin-right: 20px;
+`;
+
+const CartIconContainer = styled.div`
+    position: relative;
+    cursor: pointer;
+`;
+
+const CartCount = styled.div`
+    background: ${props => props.color};
+    border-radius: 50%;
+    padding: 4px 8px;
+    position: absolute;
+    right: 10px;
+    top: -5px;
+    font-size: 12px;
+    visibility: ${props => (props.show? "visible": "hidden")};
+`;
+
 
 class Navbar extends React.Component {
     render(){
-        return (                            //style will object with key value pair
-            <div  style={{
-                height: 70,
-                background: "#4267b2",
-                display: "flex",
-                justifyContent: "space-between" ,
-                alignItems: "center",
-                position: "relative",
-              }}>
-              
-                <div style={styles.title}> Movie -App </div>
-
-                <div style={styles.cartIconContainer}>
-                    <img alt="Cart" style={{height: 48, marginRight: 20,}}  src="https://cdn-icons-png.flaticon.com/128/891/891462.png" />
-                    <span style={styles.cartCount} > 3 </span>
-                </div>
-
-            </div>
-
-                
+        return (                          
+            <Nav>    
+                <Title> Movie App </Title>
+                <CartIconContainer>
+                    <CartImg alt="Cart-Icon" 
+                        src = "https://cdn-icons-png.flaticon.com/128/891/891462.png"    
+                    />
+                    <CartCount color="yellow" show={true}>5</CartCount>
+                </CartIconContainer>
+            </Nav>
         );
     }
 
 }
 
 export default Navbar;
-
-const styles = {
-    cartIcon: {
-        height: 48,
-        marginRight: 20,
-      },
-    //   nav: {
-    //     height: 70,
-    //     background: "#4267b2",
-    //     display: "flex",
-    //     justifyContent: "space-between" ,
-    //     alignItems: "center",
-    //     position: "relative",
-    //   },
-      title:{
-        fontSize: 30,
-        color: "#fff",
-        fontWeight: 600,
-        fontFamily: '"Montserrat", sans-serif',
-        textTransform: "uppercase",
-        marginLeft: 20
-      },
-      
-      cartIconContainer: {
-        position: "relative",
-        cursor: "pointer",
-      },
-      cartCount: {
-        background: "orange",
-        borderRadius: "50%",
-        padding: "4px 8px",
-        position: "absolute",
-        right: 10,
-        top: -5,
-        fontSize: 12,
-      },
-
-}
